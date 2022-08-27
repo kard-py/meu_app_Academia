2ced9663d5265ed81f741b996a9cf000506b9418
kard-py <caiodetz654@gmail.com>
2022-08-27 11:15:08 -0400

    adicionado texto e estilo

--- src/pages/Calendario/index.js
+++ src/pages/Calendario/index.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Calendario() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Pagina de calendario de treinos</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Pagina de calendario de treinos em desinvolvimento
+      </Text>
     </View>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import frases from "../../assets/frases.json";
 export default function Home() {
+  const [frase, setFrase] = useState("");
+  useEffect(() => {
+    setFrase(frases[Math.floor(Math.random() * frases.length)]);
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home page</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.frase}>Frase Do Dia:</Text>
+        <Text style={styles.text}>{frase}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,7 +25,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  content: {
+    width: "80%",
+    height: "100%",
+    justifyContent: "center",
+  },
   text: {
     color: "#fff",
+  },
+  frase: {
+    color: "#fff",
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+    elevation: 5,
   },
 });
